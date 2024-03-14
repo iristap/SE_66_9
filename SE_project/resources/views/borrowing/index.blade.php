2dd8c7294506dffcfc54329731c34d240d3df92a
emma ★ <104120624+chatrkml@users.noreply.github.com>
2024-03-14 21:56:47 +0700

    update borrowing/index

--- SE_project/resources/views/borrowing/index.blade.php
+++ SE_project/resources/views/borrowing/index.blade.php
@@ -7,11 +7,11 @@
                 <div class="row">
                     <div class="col-lg-12 margin-tb">
                         <div class="pull-left ">
-                            <h2>Borrowing list</h2>
+                            <br><h2>การอนุมัติการยืมครุภัณฑ์</h2>
                         </div>
 
                         <div class="pull-right ">
-                            <a class="btn btn-warning">considering</a><br><br>
+                            <a class="btn btn-warning">กำลังพิจารณา</a><br><br>
 
                             <table class="table table-bordered">
 
@@ -21,8 +21,7 @@
                                         <td>Borrower</td>
                                         <td>Borrowing Date</td>
                                         <td>Due Date</td>
-                                        <td>see the details</td>
-                                        <td>see the details</td>
+                                        <td>รายละเอียด</td>
                                     </tr>
                                 </thead>
 
@@ -36,8 +35,8 @@
                                     <td>{$brItem->users_name}</td>
                                     <td>{$brItem->borrow_date}</td>
                                     <td>{$dueDate}</td>
-                                    <td><a href='".route('borrowing.approved', ['id' => $brItem->borrowing_id])."' class='btn btn-success'>approved</a></td>
-                                    <td><a href='".route('borrowing.not_approved', ['id' => $brItem->borrowing_id])."' class='btn btn-danger'>not approved</a></td>
+                                    <td><a href='".route('borrowing.approved', ['id' => $brItem->borrowing_id])."' class='btn btn-success'>approved</a>
+                                    <a href='".route('borrowing.not_approved', ['id' => $brItem->borrowing_id])."' class='btn btn-danger'>not approved</a></td>
                                 </tr>";
                                 }
 
