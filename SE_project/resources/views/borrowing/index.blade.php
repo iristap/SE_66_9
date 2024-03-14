e1a9ce70028b822993fe4b02c7cb9a1a811e5913
emma ★ <104120624+chatrkml@users.noreply.github.com>
2024-03-15 02:29:55 +0700

    แสดงแค่สถานะ==รอ
    รักเธอแล้วใจก็มีเสียงเพลง ให้ใจครื้นเครงบรรเลงล่องไป ปลุกช้านนนนขึ้นมาใหม่ ไม่จ่ะ ปลุกไม่ได้นะจ๊ะ ยังไม่ได้นอนเลยจะตีสามโมงเช้า

--- SE_project/resources/views/borrowing/index.blade.php
+++ SE_project/resources/views/borrowing/index.blade.php
@@ -7,7 +7,8 @@
                 <div class="row">
                     <div class="col-lg-12 margin-tb">
                         <div class="pull-left ">
-                            <br><h2>การอนุมัติการยืมครุภัณฑ์</h2>
+                            <br>
+                            <h2>การอนุมัติการยืมครุภัณฑ์</h2>
                         </div>
 
                         <div class="pull-right ">
@@ -24,26 +25,27 @@
                                         <td>รายละเอียด</td>
                                     </tr>
                                 </thead>
+                                <tbody>
 
                                 <?php
                                 foreach ($borrowing as $brItem) {
-                                    $borrowDate = new DateTime($brItem->borrow_date);
-                                    $dueDate = $borrowDate->add(new DateInterval('P14D'))->format('Y-m-d');
+                                    if ($brItem->status == 'รอการอนุมัติ') {
+                                        $borrowDate = new DateTime($brItem->borrow_date);
+                                        $dueDate = $borrowDate->add(new DateInterval('P14D'))->format('Y-m-d');
 
-                                    echo "<tr>
-                                    <td>{$brItem->borrowing_id}</td>
-                                    <td>{$brItem->users_name}</td>
-                                    <td>{$brItem->borrow_date}</td>
-                                    <td>{$dueDate}</td>
-                                    <td><a href='".route('borrowing.details', ['id' => $brItem->borrowing_id])."' class='btn btn-secondary'>ดูรายละเอียด</a></td>
-                                </tr>";
+                                        echo "<tr>
+                                                                    <td>{$brItem->borrowing_id}</td>
+                                                                    <td>{$brItem->users_name}</td>
+                                                                    <td>{$brItem->borrow_date}</td>
+                                                                    <td>{$dueDate}</td>
+                                                                    <td><a href='" .
+                                            route('borrowing.details', ['id' => $brItem->borrowing_id]) .
+                                            "' class='btn btn-secondary'>ดูรายละเอียด</a></td>
+                                                                </tr>";
+                                    }
                                 }
-
-                                echo '</tbody>';
-                                echo '</table>';
                                 ?>
-
-
+                                </tbody>
                             </table>
                         </div>
                     </div>
