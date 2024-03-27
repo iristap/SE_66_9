c3df92ca8669d793953936c99b5b2cbbb31f9c27
nirobonj <nirobon.j@ku.th>
2024-03-27 21:39:28 +0700

    Update app.blade.php

--- SE_project/resources/views/layouts/app.blade.php
+++ SE_project/resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SEG9') }}</title> 
-
+    <!-- <title>{{ config('app.name', 'SEG9') }}</title>  -->
+    <title>SEG9</title>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
